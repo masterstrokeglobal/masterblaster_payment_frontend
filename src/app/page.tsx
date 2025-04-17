c2b3df2551f2844747493ad09c620892e5334848
rajas-quanto <rajas@quanto-consulting.in>
2025-04-17 15:47:22 +0530

    add logo in entry page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 import LogoText from "@/components/common/logo-text";
 import useCheckAuth from "@/hooks/use-signin-route";
+import RapidPay24Logo from "../../public/images/logo-transparent-png.png"
+import Image from "next/image";
 export default function Home() {
   useCheckAuth();
   return (
     <section className="flex justify-center items-center h-screen">
-      <LogoText className="mb-4 animate-pulse" />
+      {/* <LogoText className="mb-4 animate-pulse" /> */}
+      <Image className="mb-4 animate-pulse w-52" alt="rapidpay24" src={RapidPay24Logo} />
     </section>
   );
 }
