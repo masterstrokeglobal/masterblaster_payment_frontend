c2dde3033cf565f553f5cf7b44f654af399ae742
rajas-quanto <rajas@quanto-consulting.in>
2025-04-21 11:40:00 +0530

    ato reshresh in transaction fetch every 5 sec

--- src/app/dashboard/admin-transactions/page.tsx
+++ src/app/dashboard/admin-transactions/page.tsx
@@ -4,145 +4,174 @@ import Combobox from "@/components/ui/combobox";
 import DataTable from "@/components/ui/data-table-server";
 import { Input } from "@/components/ui/input";
 import {
-    Select,
-    SelectContent,
-    SelectGroup,
-    SelectItem,
-    SelectLabel,
-    SelectTrigger,
-    SelectValue,
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
 } from "@/components/ui/select";
 import { useGetAllTransactions } from "@/features/transaction/query/transactions-queries";
 import transactionColumns from "@/features/user/components/transaction-columns";
 import { useGetAllUsers } from "@/features/user/data/user-queries";
 import Merchant from "@/models/merchant";
-import { Transaction, TransactionStatus, TransactionType } from "@/models/transaction";
+import {
+  Transaction,
+  TransactionStatus,
+  TransactionType,
+} from "@/models/transaction";
 import { Search } from "lucide-react";
 import React, { useMemo, useState } from "react";
 
-
 const AdminTransactionTable = () => {
-    const [merchanntId, setMerchanntId] = useState<string | undefined>(undefined);
-    const [page, setPage] = useState(1);
-    const [search, setSearch] = useState("");
-    const [type, setType] = useState<string | "">("");
-    const [status, setStatus] = useState<string | "">("");
+  const [merchanntId, setMerchanntId] = useState<string | undefined>(undefined);
+  const [page, setPage] = useState(1);
+  const [search, setSearch] = useState("");
+  const [type, setType] = useState<string | "">("");
+  const [status, setStatus] = useState<string | "">("");
 
-    const { data:merchantData, isSuccess:isMerchantSuccess } = useGetAllUsers({
-        page: page,
-        search: search,
-    });
+  const { data: merchantData, isSuccess: isMerchantSuccess } = useGetAllUsers({
+    page: page,
+    search: search,
+  });
 
-    const merchantOptions = useMemo(() => {
-        if (isMerchantSuccess && merchantData?.data?.merchants) {
-            return Array.from(merchantData.data.merchants).map((user: any) => new Merchant(user)).map((merchant: Merchant) => {
-                return {
-                    value: merchant.id!.toString(),
-                    label: merchant.name!
-                }
-            });
-        }
-        return [];
-    }, [ merchantData, isMerchantSuccess]);
-    // Fetch all transactions with pagination, search query, and filters
-    const { data, isSuccess, isFetching } = useGetAllTransactions({
-        page: page,
-        search: search,
-        type: type === "all" ? "" : type,
-        merchantId: merchanntId,
-        status: status === "all" ? "" : status,
-    });
+  const merchantOptions = useMemo(() => {
+    if (isMerchantSuccess && merchantData?.data?.merchants) {
+      return Array.from(merchantData.data.merchants)
+        .map((user: any) => new Merchant(user))
+        .map((merchant: Merchant) => {
+          return {
+            value: merchant.id!.toString(),
+            label: merchant.name!,
+          };
+        });
+    }
+    return [];
+  }, [merchantData, isMerchantSuccess]);
+  // Fetch all transactions with pagination, search query, and filters
+  const { data, isSuccess, isFetching } = useGetAllTransactions(
+    {
+      page: page,
+      search: search,
+      type: type === "all" ? "" : type,
+      merchantId: merchanntId,
+      status: status === "all" ? "" : status,
+    },
+    {
+      refetchInterval: 5000,
+    }
+  );
 
-    const transactions = useMemo(() => {
-        if (isSuccess && data?.data?.transactions) {
-            return Array.from(data.data.transactions).map(
-                (transaction: any) => new Transaction(transaction)
-            );
-        }
-        return [];
-    }, [data, isSuccess]);
+  const transactions = useMemo(() => {
+    if (isSuccess && data?.data?.transactions) {
+      return Array.from(data.data.transactions).map(
+        (transaction: any) => new Transaction(transaction)
+      );
+    }
+    return [];
+  }, [data, isSuccess]);
 
-    // Calculate total pages based on data count
-    const totalPages = useMemo(() => {
-        return Math.ceil(data?.data?.total / 10) || 1;
-    }, [data, isSuccess]);
+  // Calculate total pages based on data count
+  const totalPages = useMemo(() => {
+    return Math.ceil(data?.data?.total / 10) || 1;
+  }, [data, isSuccess]);
 
-    // Handle search input change
-    const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setSearch(e.target.value);
-        setPage(1); // Reset to first page on search
-    };
+  // Handle search input change
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+    setPage(1); // Reset to first page on search
+  };
 
-    // Change page when pagination controls are used
-    const changePage = (newPage: number) => {
-        setPage(newPage);
-    };
+  // Change page when pagination controls are used
+  const changePage = (newPage: number) => {
+    setPage(newPage);
+  };
 
-    return (
-        <section className="container-main min-h-[60vh] my-12">
-            <header className="flex flex-col md:flex-row gap-4 flex-wrap md:items-center justify-between">
-                <h2 className="text-xl font-semibold">Transactions</h2>
-                <div className="flex gap-5 ">
-                    <div className="relative min-w-60 flex-1">
-                        <Search size={18} className="absolute top-2.5 left-2.5" />
-                        <Input
-                            placeholder="Search"
-                            onChange={handleSearch}
-                            className="pl-10"
-                        />
-                    </div>
-                    {/* ShadCN Select for Type Filter */}
-                    <Select value={type} onValueChange={(val) => {
-                        setType(val as TransactionType)
-                        setPage(1);
-                    }} >
-                        <SelectTrigger>
-                            <SelectValue placeholder="All Types" />
-                        </SelectTrigger>
-                        <SelectContent>
-                            <SelectGroup>
-                                <SelectLabel>Types</SelectLabel>
-                                <SelectItem value="all">All Types</SelectItem>
-                                <SelectItem value={TransactionType.DEPOSIT}>Deposit</SelectItem>
-                                <SelectItem value={TransactionType.WITHDRAWAL}>Withdrawal</SelectItem>
-                            </SelectGroup>
-                        </SelectContent>
-                    </Select>
+  return (
+    <section className="container-main min-h-[60vh] my-12">
+      <header className="flex flex-col md:flex-row gap-4 flex-wrap md:items-center justify-between">
+        <h2 className="text-xl font-semibold">Transactions</h2>
+        <div className="flex gap-5 ">
+          <div className="relative min-w-60 flex-1">
+            <Search size={18} className="absolute top-2.5 left-2.5" />
+            <Input
+              placeholder="Search"
+              onChange={handleSearch}
+              className="pl-10"
+            />
+          </div>
+          {/* ShadCN Select for Type Filter */}
+          <Select
+            value={type}
+            onValueChange={(val) => {
+              setType(val as TransactionType);
+              setPage(1);
+            }}
+          >
+            <SelectTrigger>
+              <SelectValue placeholder="All Types" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectLabel>Types</SelectLabel>
+                <SelectItem value="all">All Types</SelectItem>
+                <SelectItem value={TransactionType.DEPOSIT}>Deposit</SelectItem>
+                <SelectItem value={TransactionType.WITHDRAWAL}>
+                  Withdrawal
+                </SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
 
-                   <Combobox options={merchantOptions} value={merchanntId} onChange={(value: React.SetStateAction<string | undefined>)=>setMerchanntId(value)}  placeholder="All Merchants" />
- 
-                    {/* ShadCN Select for Status Filter */}
-                    <Select value={status} onValueChange={(val) => {
-                        setStatus(val as TransactionStatus)
-                        setPage(1);
-                    }} >
-                        <SelectTrigger>
-                            <SelectValue placeholder="All Statuses" />
-                        </SelectTrigger>
-                        <SelectContent>
-                            <SelectGroup>
-                                <SelectLabel>Status</SelectLabel>
-                                <SelectItem value="all">All Statuses</SelectItem>
-                                <SelectItem value={TransactionStatus.PENDING}>Pending</SelectItem>
-                                <SelectItem value={TransactionStatus.COMPLETED}>Completed</SelectItem>
-                                <SelectItem value={TransactionStatus.FAILED}>Failed</SelectItem>
-                            </SelectGroup>
-                        </SelectContent>
-                    </Select>
-                </div>
-            </header>
-            <main className="mt-4">
-                <DataTable
-                    page={page}
-                    loading={isFetching}
-                    columns={transactionColumns}
-                    data={transactions}
-                    totalPage={totalPages}
-                    changePage={changePage}
-                />
-            </main>
-        </section>
-    );
+          <Combobox
+            options={merchantOptions}
+            value={merchanntId}
+            onChange={(value: React.SetStateAction<string | undefined>) =>
+              setMerchanntId(value)
+            }
+            placeholder="All Merchants"
+          />
+
+          {/* ShadCN Select for Status Filter */}
+          <Select
+            value={status}
+            onValueChange={(val) => {
+              setStatus(val as TransactionStatus);
+              setPage(1);
+            }}
+          >
+            <SelectTrigger>
+              <SelectValue placeholder="All Statuses" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectLabel>Status</SelectLabel>
+                <SelectItem value="all">All Statuses</SelectItem>
+                <SelectItem value={TransactionStatus.PENDING}>
+                  Pending
+                </SelectItem>
+                <SelectItem value={TransactionStatus.COMPLETED}>
+                  Completed
+                </SelectItem>
+                <SelectItem value={TransactionStatus.FAILED}>Failed</SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        </div>
+      </header>
+      <main className="mt-4">
+        <DataTable
+          page={page}
+          loading={isFetching}
+          columns={transactionColumns}
+          data={transactions}
+          totalPage={totalPages}
+          changePage={changePage}
+        />
+      </main>
+    </section>
+  );
 };
 
 export default AdminTransactionTable;
--- src/app/dashboard/payouts/page.tsx
+++ src/app/dashboard/payouts/page.tsx
@@ -3,16 +3,20 @@
 import DataTable from "@/components/ui/data-table-server";
 import { Input } from "@/components/ui/input";
 import {
-    Select,
-    SelectContent,
-    SelectGroup,
-    SelectItem,
-    SelectLabel,
-    SelectTrigger,
-    SelectValue,
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
 } from "@/components/ui/select";
 import { Button } from "@/components/ui/button";
-import { Transaction, TransactionStatus, TransactionType } from "@/models/transaction";
+import {
+  Transaction,
+  TransactionStatus,
+  TransactionType,
+} from "@/models/transaction";
 import { Search, Plus } from "lucide-react";
 import React, { useMemo, useState } from "react";
 
@@ -23,107 +27,117 @@ import { useGetAllTransactions } from "@/features/transaction/query/transactions
 import Link from "next/link";
 
 type Props = {
-    userId?: string;
-    onCreateTransaction?: () => void;
+  userId?: string;
+  onCreateTransaction?: () => void;
 };
 
 const TransactionTable = () => {
-    const [page, setPage] = useState(1);
-    const [search, setSearch] = useState("");
-    const [status, setStatus] = useState<string | "">("");
-    const { userDetails } = useAuthStore();
+  const [page, setPage] = useState(1);
+  const [search, setSearch] = useState("");
+  const [status, setStatus] = useState<string | "">("");
+  const { userDetails } = useAuthStore();
 
-    const merchantId = userDetails?.role === AdminRole.Merchant ? userDetails?.id : undefined;
+  const merchantId =
+    userDetails?.role === AdminRole.Merchant ? userDetails?.id : undefined;
 
-    const { data, isSuccess, isFetching } = useGetAllTransactions({
-        page: page,
-        search: search,
-        type: TransactionType.WITHDRAWAL,
-        merchantId: merchantId,
-        status: status === "all" ? "" : status,
-    });
+  const { data, isSuccess, isFetching } = useGetAllTransactions(
+    {
+      page: page,
+      search: search,
+      type: TransactionType.WITHDRAWAL,
+      merchantId: merchantId,
+      status: status === "all" ? "" : status,
+    },
+    {
+      refetchInterval: 5000,
+    }
+  );
 
-    const transactions = useMemo(() => {
-        if (isSuccess && data?.data?.transactions) {
-            return Array.from(data.data.transactions).map(
-                (transaction: any) => new Transaction(transaction)
-            );
-        }
-        return [];
-    }, [data, isSuccess]);
+  const transactions = useMemo(() => {
+    if (isSuccess && data?.data?.transactions) {
+      return Array.from(data.data.transactions).map(
+        (transaction: any) => new Transaction(transaction)
+      );
+    }
+    return [];
+  }, [data, isSuccess]);
 
-    const totalPages = useMemo(() => {
-        return Math.ceil(data?.data?.total / 10) || 1;
-    }, [data, isSuccess]);
+  const totalPages = useMemo(() => {
+    return Math.ceil(data?.data?.total / 10) || 1;
+  }, [data, isSuccess]);
 
-    const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setSearch(e.target.value);
-        setPage(1);
-    };
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+    setPage(1);
+  };
 
-    const changePage = (newPage: number) => {
-        setPage(newPage);
-    };
+  const changePage = (newPage: number) => {
+    setPage(newPage);
+  };
 
-    return (
-        <section className="container-main min-h-[60vh] my-12">
-            <header className="flex flex-col md:flex-row gap-4 flex-wrap md:items-center justify-between">
-                <div className="flex items-center gap-4">
-                    <h2 className="text-xl font-semibold">Withdrawal Requests</h2>
+  return (
+    <section className="container-main min-h-[60vh] my-12">
+      <header className="flex flex-col md:flex-row gap-4 flex-wrap md:items-center justify-between">
+        <div className="flex items-center gap-4">
+          <h2 className="text-xl font-semibold">Withdrawal Requests</h2>
+        </div>
+        <div className="flex gap-5">
+          <div className="relative min-w-60 flex-1">
+            <Search size={18} className="absolute top-2.5 left-2.5" />
+            <Input
+              placeholder="Search"
+              onChange={handleSearch}
+              className="pl-10"
+            />
+          </div>
 
-                </div>
-                <div className="flex gap-5">
-                    <div className="relative min-w-60 flex-1">
-                        <Search size={18} className="absolute top-2.5 left-2.5" />
-                        <Input
-                            placeholder="Search"
-                            onChange={handleSearch}
-                            className="pl-10"
-                        />
-                    </div>
-
-                    {/* Status Filter */}
-                    <Select value={status} onValueChange={(val) => {
-                        setStatus(val as TransactionStatus)
-                        setPage(1);
-                    }} >
-                        <SelectTrigger>
-                            <SelectValue placeholder="All Statuses" />
-                        </SelectTrigger>
-                        <SelectContent>
-                            <SelectGroup>
-                                <SelectLabel>Status</SelectLabel>
-                                <SelectItem value="all">All Statuses</SelectItem>
-                                <SelectItem value={TransactionStatus.PENDING}>Pending</SelectItem>
-                                <SelectItem value={TransactionStatus.COMPLETED}>Completed</SelectItem>
-                                <SelectItem value={TransactionStatus.FAILED}>Failed</SelectItem>
-                            </SelectGroup>
-                        </SelectContent>
-                    </Select>
-                    {userDetails?.isMerchant &&
-                        <Link href="/dashboard/payouts/create">
-                            <Button
-                                className="flex items-center gap-2"
-                            >
-                                <Plus size={16} />
-                                Create Request
-                            </Button>
-                        </Link>
-                    }
-                </div>
-            </header>
-            <main className="mt-4">
-                <DataTable
-                    page={page}
-                    loading={isFetching}
-                    columns={transactionColumns}
-                    data={transactions}
-                    totalPage={totalPages}
-                    changePage={changePage}
-                />
-            </main>
-        </section>
-    );
+          {/* Status Filter */}
+          <Select
+            value={status}
+            onValueChange={(val) => {
+              setStatus(val as TransactionStatus);
+              setPage(1);
+            }}
+          >
+            <SelectTrigger>
+              <SelectValue placeholder="All Statuses" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectLabel>Status</SelectLabel>
+                <SelectItem value="all">All Statuses</SelectItem>
+                <SelectItem value={TransactionStatus.PENDING}>
+                  Pending
+                </SelectItem>
+                <SelectItem value={TransactionStatus.COMPLETED}>
+                  Completed
+                </SelectItem>
+                <SelectItem value={TransactionStatus.FAILED}>Failed</SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+          {userDetails?.isMerchant && (
+            <Link href="/dashboard/payouts/create">
+              <Button className="flex items-center gap-2">
+                <Plus size={16} />
+                Create Request
+              </Button>
+            </Link>
+          )}
+        </div>
+      </header>
+      <main className="mt-4">
+        <DataTable
+          page={page}
+          loading={isFetching}
+          columns={transactionColumns}
+          data={transactions}
+          totalPage={totalPages}
+          changePage={changePage}
+        />
+      </main>
+    </section>
+  );
 };
 
 export default TransactionTable;
